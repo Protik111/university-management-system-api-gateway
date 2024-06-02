40817af643080d4d88b8f1b47b419b8caf14ec01
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-06-02 08:40:45 +0600

    [DONE] redis client

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,17 @@ import routes from './app/routes';
 
 const app: Application = express();
 
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/v1', routes);
+// app.use('/api/v1', routes);
 
 app.use(globalExceptionHandler);
+app.get('/', (req, res) => {
+  console.log('Server is running');
+});
 
 app.use((req, res, next) => {
   res.status(httpStatus.NOT_FOUND).json({
