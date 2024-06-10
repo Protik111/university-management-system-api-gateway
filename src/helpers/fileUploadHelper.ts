ecd45bf9a998da3c01213dfbb0f81b4a7a5fc5b5
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-06-10 22:18:40 +0600

    [DONE] file uploader middleware

---
+++ src/helpers/fileUploadHelper.ts
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from 'cloudinary';
+import multer from 'multer';
+import * as fs from 'fs';
+import { ICloudinaryResponse, IUploadFile } from '../interfaces/file';
+
+cloudinary.config({
+  cloud_name: 'dukinbgee',
+  api_key: '177946576474248',
+  api_secret: '9_jFRnOEilzJ3pYi4n1PYbf-39A'
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+const uploadToCloudinary = async (file: IUploadFile): Promise<ICloudinaryResponse | undefined> => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(file.path, (error: Error, result: ICloudinaryResponse) => {
+      fs.unlinkSync(file.path);
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+export const fileUploadHelper = {
+  uploadToCloudinary,
+  upload
+};
